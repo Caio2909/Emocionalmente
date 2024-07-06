f41907008d992238561ee7ad87d022b7b01542d0
Caio R <114326411+Caio2909@users.noreply.github.com>
2024-07-06 18:45:47 -0300

    Add files via upload

--- criarAvatar.js
+++ criarAvatar.js
@@ -1,15 +1,59 @@
+sombrancelhas = ["sadConcerned", "default", "angry"]
+olhos = ["cry", "default", "squint"] // 0 - triste 1 - normal 2 - raiva
+boca = ["screamOpen", "default", "grimace"]
+
 const avatar = document.getElementById('avatar');
 const skinButtons = document.querySelectorAll('.skinCorButton');
 const hairButtons = document.querySelectorAll('.hairCorButton');
-const hairColor = document.getElementById('hairCor');
-let selectedSkinColor = 'ffdbac'; 
+const usaOculos = document.querySelectorAll('.oculosInput')
+const sentimento = document.querySelectorAll('.sentimentoInput')
+const hairType = document.getElementById('hairType');
+
+let selectedSkinColor = 'ffdbac'
 let selectedHairColor = 'a2826D'
+let selectedOculos = ''
+let selectedSentimento = 1
+let booleanOculos = 0
+let selectedHairType = "curvy"
+
 function updateAvatar() {
-    const link = `https://api.dicebear.com/9.x/avataaars/svg?skinColor=${selectedSkinColor}&hairColor=${selectedHairColor}`;
+    const link = `https://api.dicebear.com/9.x/avataaars/svg?accessories=
+${selectedOculos}
+&hairColor=${selectedHairColor}
+&skinColor=${selectedSkinColor}
+&mouth=${boca[selectedSentimento]}
+&eyes=${olhos[selectedSentimento]}
+&accessoriesProbability=${booleanOculos}&accessoriesColor=262e33
+&eyebrows=${sombrancelhas[selectedSentimento]}
+&top=${selectedHairType}`;
     avatar.src = link;
 }
 
-// Adiciona evento de clique a cada botão de cor da pele
+hairType.addEventListener('change', (event) => {
+    selectedHairType = event.target.value;
+    updateAvatar();
+});
+
+sentimento.forEach(input => {
+    input.addEventListener('click', (event) => {
+        selectedSentimento = event.target.value;
+        updateAvatar();
+    })
+})
+
+usaOculos.forEach(input => {
+    input.addEventListener('click', (event) => {
+        selectedOculos = event.target.value;
+        if (selectedOculos == ''){
+            booleanOculos = 0
+        }
+        else{
+            booleanOculos = 100
+        }
+        updateAvatar();
+    })
+})
+
 skinButtons.forEach(button => {
     button.addEventListener('click', (event) => {
         selectedSkinColor = event.target.value;
@@ -22,5 +66,5 @@ hairButtons.forEach(button =>{
         updateAvatar();
     });
 });
-// Inicializa o avatar na carga da página
+
 window.onload = updateAvatar;
--- logado.php
+++ logado.php
@@ -11,26 +11,48 @@
     <img height="200px" id="avatar" />
   </div>
 
-  <div id="colorButtons">
+  <div id="colorButtons"><!-- Pele -->
     <label for="skinColorSelect">Escolha sua cor de pele:</label><br>
-    <button class="skinCorButton colorButton" value="ffdbac" style="background-color:#ffdbac;"></button>
-    <button class="skinCorButton colorButton" value="d8b294" style="background-color:#d8b294;"></button>
-    <button class="skinCorButton colorButton" value="f1c27d" style="background-color:#f1c27d;"></button>
-    <button class="skinCorButton colorButton" value="8d5524" style="background-color:#8d5524;"></button>
-    <button class="skinCorButton colorButton" value="614335" style="background-color:#614335;"></button>
+    <button type="button" class="skinCorButton colorButton" value="ffdbac" style="background-color:#ffdbac;"></button>
+    <button type="button" class="skinCorButton colorButton" value="d8b294" style="background-color:#d8b294;"></button>
+    <button type="button" class="skinCorButton colorButton" value="f1c27d" style="background-color:#f1c27d;"></button>
+    <button type="button" class="skinCorButton colorButton" value="8d5524" style="background-color:#8d5524;"></button>
+    <button type="button" class="skinCorButton colorButton" value="614335" style="background-color:#614335;"></button>
   </div>
 
-  <br><br>
+  <div id="colorButtons"><!-- Cabelo-->
+    <label for="hairTypeSelect">Como é seu cabelo?</label><br>
+    <select class="hairType" id="hairType">
+        <option value="curvy">Curvo</option>
+        <option value="curly">Cacheado</option>
+        <option value="longButNotTooLong">Médio</option>
+        <option value="straight02">Liso</option>
+    </select>
+  </div>
 
-  <div id="colorButtons">
+  <div id="colorButtons"><!-- Cor cabelo-->
     <label for="hairColorSelect">E qual a cor do seu cabelo?</label><br>
-    <button class="hairCorButton colorButton" value="a2826D" style="background-color:#a2826D;"></button>
-    <button class="hairCorButton colorButton" value="2D221C" style="background-color:#2D221C;"></button>
-    <button class="hairCorButton colorButton" value="785C4E" style="background-color:#785C4E;"></button>
-    <button class="hairCorButton colorButton" value="542217" style="background-color:#542217;"></button>
-    <button class="hairCorButton colorButton" value="D19F7E" style="background-color:#D19F7E;"></button>
+    <button type="button" class="hairCorButton colorButton" value="D19F7E" style="background-color:#D19F7E;"></button>
+    <button type="button" class="hairCorButton colorButton" value="a2826D" style="background-color:#a2826D;"></button>
+    <button type="button" class="hairCorButton colorButton" value="785C4E" style="background-color:#785C4E;"></button>
+    <button type="button" class="hairCorButton colorButton" value="b38b67" style="background-color:#b38b67;"></button>
+    <button type="button" class="hairCorButton colorButton" value="2D221C" style="background-color:#2D221C;"></button>
   </div>
 
+  <div id="colorButtons"><!-- Oculos-->
+    <label for="oculos">Voce usa óculos?</label><br>
+    <input type="radio" id="oculos" value="prescription01" name="oculos" class="oculosInput">Sim
+    <input type="radio" id="oculos" value="" name="oculos" class="oculosInput">Não
+
+  </div>
+  <div id="colorButtons"><!-- Sentimento-->
+    <label for="sentimentos">Como você está se sentindo?</label><br>
+    <input type="radio" value="0" name="sentimento" class="sentimentoInput">Triste🙁
+    <input type="radio" value="1" name="sentimento" class="sentimentoInput">Feliz 😊
+    <input type="radio" value="2" name="sentimento" class="sentimentoInput">Raiva 😡
+
+
+  </div>
   <script src="criarAvatar.js"></script>
 </body>
 </html>
