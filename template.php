d3ecd37488828708bce830211f3317fe80aac2ad
Caio R <114326411+Caio2909@users.noreply.github.com>
2024-07-18 22:35:37 -0300

    Add files via upload

---
+++ template.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>template</title>
+    <link rel="stylesheet" href="styleTemplate.css">
+    <div class="header">
+            <img src="imgs/image.png" >
+            <h1>EMOCIONALMENTE</h1>
+        </div>
+        <a href="linkproxpag" class="button">Acesso Criança</a>
+</head>
+<body>   
+</body>
+</html>
