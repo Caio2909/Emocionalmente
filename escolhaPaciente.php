b189c6ed9d2bb46b4316ef89906ae2c030d0455a
Caio R <114326411+Caio2909@users.noreply.github.com>
2024-07-25 17:48:24 -0300

    Add files via upload

---
+++ escolhaPaciente.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela de Pacientes</title>
+    <link rel="stylesheet" href="stylesTable.css">
+</head>
+<body>
+    <div class="header">
+        <img src="imgs/image.png" alt="Logo">
+        <h1>EMOCIONALMENTE</h1>
+    </div>
+    <div class="container">
+        <table id="patientsTable">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Idade</th>
+                    <th>Última Consulta</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- dados dos pacientes -->
+            </tbody>
+        </table>
+    </div>
+    <script src="scriptTable.js"></script>
+</body>
+
+</html>
--- template.php
+++ template.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>template</title>
-    <link rel="stylesheet" href="styleTemplate.css">
-    <div class="header">
+    <link rel="stylesheet" href="styleTemplate.css">   
+</head>
+<header><div class="header">
             <img src="imgs/image.png" >
             <h1>EMOCIONALMENTE</h1>
         </div>
         <a href="linkproxpag" class="button">Acesso Criança</a>
-</head>
+</header>
 <body>   
 </body>
 </html>
